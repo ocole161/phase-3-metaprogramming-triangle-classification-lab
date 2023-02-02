b441f2dc99046153c496a92980b0730bc07e64d4
Owen <ocole161@gmail.com>
2023-02-02 15:51:28 -0700

    commit tree c4e13881e9ac336c4ca493c8c5cd57e270509f54

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def isInvalid?
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
+      true
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side3 + @side2 <= @side1)
+      true
+    end
+  end
+
+  def kind
+    if
+      isInvalid?
+      raise TriangleError
+    elsif (@side1 == @side2) && (@side2 == @side3)
+      :equilateral
+    elsif (@side1 == @side2 && @side2 != @side3) || (@side3 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side2 != @side3)
+    :isosceles  
+    elsif (@side1 != @side2) && (@side2 != @side3)
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
